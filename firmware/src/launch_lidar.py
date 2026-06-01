ba431e8cc2e413b5b71af4390234c0971d4ad697
joevento <joe.te.vento@gmail.com>
2026-06-01 13:57:50 +0300

    Slight rework of configs to allow both lidars to be displayed simultaniously

--- firmware/src/launch_lidar.py
+++ firmware/src/launch_lidar.py
@@ -1,17 +1,40 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    rviz_config=get_package_share_directory('hesai_ros_driver')+'/rviz/rviz2.rviz'
-    yaml_config='/ros2_ws/config/config_xt.yaml'
+    rviz_config='/ros2_ws/config/rviz2.rviz'
+    yaml_config_xt='/ros2_ws/config/config_xt.yaml'
+    yaml_config_jt='/ros2_ws/config/config_jt.yaml'
     return LaunchDescription([
         Node(
-            namespace='hesai_ros_driver', 
-            package='hesai_ros_driver', 
-            executable='hesai_ros_driver_node', 
+            namespace='hesai_ros_driver_xt',
+            package='hesai_ros_driver',
+            executable='hesai_ros_driver_node',
             output='screen',
-            parameters=[{'config_path': yaml_config}]
+            parameters=[{'config_path': yaml_config_xt}]
         ),
-        Node(namespace='rviz2', package='rviz2', executable='rviz2', arguments=['-d',rviz_config])
+        Node(
+            namespace='hesai_ros_driver_jt',
+            package='hesai_ros_driver',
+            executable='hesai_ros_driver_node',
+            output='screen',
+            parameters=[{'config_path': yaml_config_jt}]
+        ),
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            arguments=['--x', '0', '--y', '0', '--z', '0',
+                       '--roll', '0', '--pitch', '0', '--yaw', '0',
+                       '--frame-id', 'base_link',
+                       '--child-frame-id', 'hesai_lidar_xt']
+        ),
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            arguments=['--x', '0', '--y', '0', '--z', '0',
+                       '--roll', '0', '--pitch', '0', '--yaw', '0',
+                       '--frame-id', 'base_link',
+                       '--child-frame-id', 'hesai_lidar_jt']
+        ),
+        Node(namespace='rviz2', package='rviz2', executable='rviz2', arguments=['-d', rviz_config])
     ])
