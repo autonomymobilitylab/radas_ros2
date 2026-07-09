c9fd839b3a3497c0d9118e60168c38eb8781f67c
joevento <joe.te.vento@gmail.com>
2026-07-09 10:24:52 +0300

    implement bringup for basler cameras

--- firmware/src/cameras/cameranode.py
+++ firmware/src/cameras/cameranode.py
@@ -11,7 +11,7 @@ from std_msgs.msg import Bool
 
 class ImageSaver(Node):
     def __init__(self, save_directory: str, camera_name: str):
-        super().__init__("image_saver")
+        super().__init__(f"{camera_name}_image_saver")
         self.bridge = CvBridge()
 
         self.save_dir = save_directory
@@ -21,10 +21,10 @@ class ImageSaver(Node):
         self.topic_dir = os.path.join(self.save_dir, self.camera_name)
         os.makedirs(self.topic_dir, exist_ok=True)
 
-        self.collection_active = False
-        self.collection_state_sub = self.create_subscription(
-            Bool, "/data_collection_active", self.collection_state_callback, 10
-        )
+        #self.collection_active = False
+        #self.collection_state_sub = self.create_subscription(
+        #    Bool, "/data_collection_active", self.collection_state_callback, 10
+        #)
 
         self.subscription = self.create_subscription(
             Image,
@@ -34,20 +34,20 @@ class ImageSaver(Node):
         )
         self.count = 0
 
-    def collection_state_callback(self, msg: Bool):
-        self.collection_active = msg.data
-        if self.collection_active:
-            self.get_logger().info(f"Data collection activated for {self.camera_name}.")
-        else:
-            self.get_logger().info(
-                f"Data collection deactivated for {self.camera_name}."
-            )
+    #def collection_state_callback(self, msg: Bool):
+    #    self.collection_active = msg.data
+    #    if self.collection_active:
+    #        self.get_logger().info(f"Data collection activated for {self.camera_name}.")
+    #    else:
+    #        self.get_logger().info(
+    #            f"Data collection deactivated for {self.camera_name}."
+    #        )
 
     def image_callback(self, msg):
-        if not self.collection_active:
-            return
+        #if not self.collection_active:
+        #    return
 
-        cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="rgb8")
+        cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
         filename = f"image_{self.count:06d}.png"
         filepath = os.path.join(self.topic_dir, filename)
         suc = cv2.imwrite(filepath, cv_image)
--- firmware/src/radas_bringup/launch/bringup.launch.py
+++ firmware/src/radas_bringup/launch/bringup.launch.py
@@ -1,43 +1,88 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
 
 from ament_index_python.packages import get_package_share_directory
 import os
 
 
 def generate_launch_description():
+    left_data_collection = ExecuteProcess(
+        cmd=[
+            "/ros2_ws/src/.venv/bin/python3",
+            "/ros2_ws/src/cameras/cameranode.py",
+            "--camera-name=Basler_left",
+        ],
+        cwd="/ros2_ws",
+    )
+    middle_data_collection = ExecuteProcess(
+        cmd=[
+            "/ros2_ws/src/.venv/bin/python3",
+            "/ros2_ws/src/cameras/cameranode.py",
+            "--camera-name=Basler_middle",
+        ],
+        cwd="/ros2_ws",
+    )
+    right_data_collection = ExecuteProcess(
+        cmd=[
+            "/ros2_ws/src/.venv/bin/python3",
+            "/ros2_ws/src/cameras/cameranode.py",
+            "--camera-name=Basler_right",
+        ],
+        cwd="/ros2_ws",
+    )
+
     nodes = [
         Node(
-            namespace="radas_xt",
-            package="hesai_ros_driver",
-            executable="hesai_ros_driver_node",
-            name="xt32",
+            namespace="Basler_left",
+            package="pylon_ros2_camera_wrapper",
+            executable="pylon_ros2_camera_wrapper",
+            name="pylon_ros2_camera_node",
             output="screen",
+            emulate_tty=True,
             parameters=[
+                "/ros2_ws/config/basler_left.yaml",
                 {
-                    "config_path": os.path.join(
-                        get_package_share_directory("radas_bringup"),
-                        "config",
-                        "xt32.yaml",
-                    )
-                }
+                    "startup_user_set": "CurrentSetting",
+                    "enable_status_publisher": False,
+                    "enable_current_params_publisher": False,
+                },
             ],
         ),
         Node(
-            namespace="radas_jt",
-            package="hesai_ros_driver",
-            executable="hesai_ros_driver_node",
-            name="jt128",
+            namespace="Basler_middle",
+            package="pylon_ros2_camera_wrapper",
+            executable="pylon_ros2_camera_wrapper",
+            name="pylon_ros2_camera_node",
             output="screen",
+            emulate_tty=True,
             parameters=[
+                "/ros2_ws/config/basler_middle.yaml",
                 {
-                    "config_path": os.path.join(
-                        get_package_share_directory("radas_bringup"),
-                        "config",
-                        "jt128.yaml",
-                    )
-                }
+                    "startup_user_set": "CurrentSetting",
+                    "enable_status_publisher": False,
+                    "enable_current_params_publisher": False,
+                },
             ],
         ),
+        Node(
+            namespace="Basler_right",
+            package="pylon_ros2_camera_wrapper",
+            executable="pylon_ros2_camera_wrapper",
+            name="pylon_ros2_camera_node",
+            output="screen",
+            emulate_tty=True,
+            parameters=[
+                "/ros2_ws/config/basler_right.yaml",
+                {
+                    "startup_user_set": "CurrentSetting",
+                    "enable_status_publisher": True,
+                    "enable_current_params_publisher": False,
+                },
+            ],
+        ),
+        left_data_collection,
+        middle_data_collection,
+        right_data_collection,
     ]
     return LaunchDescription(nodes)
