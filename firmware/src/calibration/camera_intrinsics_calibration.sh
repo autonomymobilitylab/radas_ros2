cbbf42b44d2610fd7083390f15bcfff47391ea74
joevento <joe.te.vento@gmail.com>
2026-07-16 12:14:49 +0300

    Automatically move instrinsics calibration results to correct folder in config

--- firmware/src/calibration/camera_intrinsics_calibration.sh
+++ firmware/src/calibration/camera_intrinsics_calibration.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-read -p "Enter camera name to calculate intrinsics for: " camera_name
-ros2 run camera_calibration cameracalibrator --size 8x11 --square 0.60 --no-service-check image:=$camera_name/pylon_ros2_camera_node/image_raw camera:=/$camera_name/pylon_ros2_camera_node
+read -p "Enter camera position ['left', 'middle', 'right'] to calculate intrinsics for: " camera_position
+ros2 run camera_calibration cameracalibrator --size 8x11 --square 0.60 --no-service-check image:=Basler_$camera_position/pylon_ros2_camera_node/image_raw camera:=/Basler_$camera_position/pylon_ros2_camera_node
+read -p "Calibration complete. Press any key to override the existing calibration file, or Ctrl+C to cancel." -n1 -s
+tar -xzf /tmp/calibrationdata.tar.gz -C /ros2_ws/config/instrinsics/$camera_position/
