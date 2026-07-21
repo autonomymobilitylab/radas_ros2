39d147927534fcaa1ec13de2ef5c12f9ac9eee45
joevento <joe.te.vento@gmail.com>
2026-07-21 10:42:18 +0300

    Add gps config to the appropriate place

--- firmware/src/testing/launch_gps.sh
+++ firmware/src/testing/launch_gps.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-cp /ros2_ws/config/custom_rover.yaml /opt/ros/jazzy/share/septentrio_gnss_driver/config/custom_rover.yaml
 ros2 launch septentrio_gnss_driver rover.launch.py file_name:=custom_rover.yaml
