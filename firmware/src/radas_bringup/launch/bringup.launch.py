8394c7c7af61eaf1f733aabebcf7a20345417550
joevento <joe.te.vento@gmail.com>
2026-07-21 12:59:16 +0300

    fix bringup and camera configs in webui

--- firmware/src/radas_bringup/launch/bringup.launch.py
+++ firmware/src/radas_bringup/launch/bringup.launch.py
@@ -96,54 +96,86 @@ def generate_launch_description():
     )
 
     nodes = [
-        #Node(
-        #    namespace="Basler_left",
-        #    package="pylon_ros2_camera_wrapper",
-        #    executable="pylon_ros2_camera_wrapper",
-        #    name="pylon_ros2_camera_node",
-        #    output="screen",
-        #    emulate_tty=True,
-        #    parameters=[
-        #        "/ros2_ws/config/basler_left.yaml",
-        #        {
-        #            "startup_user_set": "CurrentSetting",
-        #            "enable_status_publisher": False,
-        #            "enable_current_params_publisher": False,
-        #        },
-        #    ],
-        #),
-        #Node(
-        #    namespace="Basler_middle",
-        #    package="pylon_ros2_camera_wrapper",
-        #    executable="pylon_ros2_camera_wrapper",
-        #    name="pylon_ros2_camera_node",
-        #    output="screen",
-        #    emulate_tty=True,
-        #    parameters=[
-        #        "/ros2_ws/config/basler_middle.yaml",
-        #        {
-        #            "startup_user_set": "CurrentSetting",
-        #            "enable_status_publisher": False,
-        #            "enable_current_params_publisher": False,
-        #        },
-        #    ],
-        #),
-        #Node(
-        #    namespace="Basler_right",
-        #    package="pylon_ros2_camera_wrapper",
-        #    executable="pylon_ros2_camera_wrapper",
-        #    name="pylon_ros2_camera_node",
-        #    output="screen",
-        #    emulate_tty=True,
-        #    parameters=[
-        #        "/ros2_ws/config/basler_right.yaml",
-        #        {
-        #            "startup_user_set": "CurrentSetting",
-        #            "enable_status_publisher": True,
-        #            "enable_current_params_publisher": False,
-        #        },
-        #    ],
-        #),
+        Node(
+            namespace="Basler_left",
+            package="pylon_ros2_camera_wrapper",
+            executable="pylon_ros2_camera_wrapper",
+            name="pylon_ros2_camera_node",
+            output="screen",
+            emulate_tty=True,
+            parameters=[
+                "/ros2_ws/config/basler_left.yaml",
+                {
+                    "startup_user_set": "CurrentSetting",
+                    "enable_status_publisher": False,
+                    "enable_current_params_publisher": False,
+                },
+            ],
+        ),
+        Node(
+            namespace="Basler_middle",
+            package="pylon_ros2_camera_wrapper",
+            executable="pylon_ros2_camera_wrapper",
+            name="pylon_ros2_camera_node",
+            output="screen",
+            emulate_tty=True,
+            parameters=[
+                "/ros2_ws/config/basler_middle.yaml",
+                {
+                    "startup_user_set": "CurrentSetting",
+                    "enable_status_publisher": False,
+                    "enable_current_params_publisher": False,
+                },
+            ],
+        ),
+        Node(
+            namespace="Basler_right",
+            package="pylon_ros2_camera_wrapper",
+            executable="pylon_ros2_camera_wrapper",
+            name="pylon_ros2_camera_node",
+            output="screen",
+            emulate_tty=True,
+            parameters=[
+                "/ros2_ws/config/basler_right.yaml",
+                {
+                    "startup_user_set": "CurrentSetting",
+                    "enable_status_publisher": True,
+                    "enable_current_params_publisher": False,
+                },
+            ],
+        ),
+        Node(
+            namespace="radas_xt",
+            package="hesai_ros_driver",
+            executable="hesai_ros_driver_node",
+            name="xt32",
+            output="screen",
+            parameters=[
+                {
+                    "config_path": os.path.join(
+                        get_package_share_directory("radas_bringup"),
+                        "config",
+                        "xt32.yaml",
+                    )
+                }
+            ],
+        ),
+        Node(
+            namespace="radas_jt",
+            package="hesai_ros_driver",
+            executable="hesai_ros_driver_node",
+            name="jt128",
+            output="screen",
+            parameters=[
+                {
+                    "config_path": os.path.join(
+                        get_package_share_directory("radas_bringup"),
+                        "config",
+                        "jt128.yaml",
+                    )
+                }
+            ],
+        ),
         Node(
             package="diagnostic_aggregator",
             executable="aggregator_node",
