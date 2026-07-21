abc365c5dbded0cba7df40afd7d993684b63d85c
joevento <joe.te.vento@gmail.com>
2026-07-21 14:54:16 +0300

    The gnss board needs an wifi extension addon to be able to use built in ntrip client. So we will broadcast the client data from the NUC instead

--- firmware/src/testing/launch_gps.sh
+++ firmware/src/testing/launch_gps.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+source /ros2_ws/src/.env
+
+str2str -in "ntrip://${NTRIPUSER}:${NTRIPPASS}@opencaster.nls.fi:2101/VRS-FKP" -out "serial://ttyACM0:115200:8:n:1:of" -p 60.188333 24.823917 4 -n 5000 -t 3 &
 ros2 launch septentrio_gnss_driver rover.launch.py file_name:=custom_rover.yaml
