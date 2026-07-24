1109060a43b87a6d9dac647b084cd257478e3190
AMlab laptop <peltonen.k.otto@gmail.com>
2026-07-24 16:39:15 +0300

    Implemented using an initial guess in the calibration

--- firmware/src/calibration/launch_extrinsic_calibration.sh
+++ firmware/src/calibration/launch_extrinsic_calibration.sh
@@ -36,6 +36,8 @@ case $CALIB_TYPE in
         CAMERA_ID=""
         RESULT_DST=""
         RESULT_SRC=""
+        TF_PID=""
+
         while :; do
             read -r -p "Choose camera to calibrate ['left', 'middle', 'right']: " CAMERA_POS 
             case "$CAMERA_POS" in
@@ -44,6 +46,9 @@ case $CALIB_TYPE in
                     CAMERA_ID="camera_left"
                     RESULT_SRC="/calib_ws/camera_left_xt32_extrinsic_calibration/calibration_results.txt"
                     RESULT_DST="/ros2_ws/config/extrinsics/left/last_left_calib.yaml"
+
+                    ros2 run tf2_ros static_transform_publisher 0.339415 -0.042709 -0.342209 -2.50087 0.00247445 -1.57505 hesai_lidar_xt camera_left &
+                    TF_PID=$!
                     break
                     ;;
                 middle)
@@ -51,6 +56,8 @@ case $CALIB_TYPE in
                     CAMERA_ID="camera_middle"
                     RESULT_SRC="/calib_ws/camera_middle_xt32_extrinsic_calibration/calibration_results.txt"
                     RESULT_DST="/ros2_ws/config/extrinsics/middle/last_middle_calib.yaml"
+
+                    ros2 run tf2_ros static_transform_publisher -0.0268913 -0.0651083 -0.368539 -3.11717 -0.00739092 -1.55419 hesai_lidar_xt camera_middle &
                     break
                     ;;
                 right)
@@ -58,6 +65,8 @@ case $CALIB_TYPE in
                     CAMERA_ID="camera_right"
                     RESULT_SRC="/calib_ws/camera_right_xt32_extrinsic_calibration/calibration_results.txt"
                     RESULT_DST="/ros2_ws/config/extrinsics/right/last_right_calib.yaml"
+
+                    ros2 run tf2_ros static_transform_publisher -0.326422 -0.0613683 -0.415493 2.52395 0.0103327 -1.55109 hesai_lidar_xt camera_right &
                     break
                     ;;
                 *) 
@@ -76,13 +85,25 @@ case $CALIB_TYPE in
         -p image_state:="UNDISTORTED" \
         -p lidar_sensor_name:="xt32" \
         -p lidar_cloud_topic:="/lidar_points_xt" \
-        -p use_initial_guess:=false
+            -p use_initial_guess:=true &
 
         CALIB_PID=$!
         wait "$CALIB_PID"
 
+        if [ -n "$TF_PID" ]; then
+            kill "$TF_PID" 2>/dev/null
+            wait "$TF_PID" 2>/dev/null
+        fi
+
+        mkdir -p "$(dirname "$RESULT_DST")"
+
         if [ -f "$RESULT_SRC" ]; then
             cp "$RESULT_SRC" "$RESULT_DST"
+
+            sed -E -i \
+            's/^([[:space:]]*>[[:space:]]*RPY:[[:space:]]*)([^[:space:]]+)([[:space:]]+)([^[:space:]]+)([[:space:]]+)([^[:space:]]+)/\1\6\3\4\5\2/' \
+            "$RESULT_DST"
+ 
 
             echo "Copied calibration result:"
             echo "  from: $RESULT_SRC"
