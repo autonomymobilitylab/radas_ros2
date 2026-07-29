db98e23838774b70b810b3900e1b0ffd5bb39dc2
AMlab laptop <peltonen.k.otto@gmail.com>
2026-07-29 15:41:34 +0300

    added camera_info bagging

--- firmware/config/rosbag_qos/system_bag.sh
+++ firmware/config/rosbag_qos/system_bag.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 ros2 bag record \
-  --qos-profile-overrides-path qos_overrides.yaml \
   /Basler_left/pylon_ros2_camera_node/image_raw \
   /Basler_middle/pylon_ros2_camera_node/image_raw \
   /Basler_right/pylon_ros2_camera_node/image_raw \
+  /Basler_left/pylon_ros2_camera_node/camera_info \
+  /Basler_middle/pylon_ros2_camera_node/camera_info \
+  /Basler_right/pylon_ros2_camera_node/camera_info \
   /lidar_points_xt \
   /lidar_points_jt \
   /imu/acceleration_hr \
